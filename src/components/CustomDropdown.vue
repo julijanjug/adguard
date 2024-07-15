8d654fa3a7b6146aaa5998dd4016f90be4360edc
name <your_email@celtra.com>
2024-07-15 12:58:09 +0200

    Adds custom dropdown

---
+++ src/components/CustomDropdown.vue
@@ -0,0 +1,148 @@
+<template>
+    <div class="select-container w-full">
+        <div class="custom-select rounded-lg h-11" @click="toggleDropdown">
+            <div class="selected-option">
+                {{ selectedOption ? selectedOption.label : 'Select Guideline' }}
+            </div>
+            <div class="carrot-icon" :class="{ 'rotate-90': !isOpen }">
+                <svg class="h-3 w-3" viewBox="0 0 10 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" clip-rule="evenodd"
+                        d="M0.646447 0.646447C0.841709 0.451184 1.15829 0.451184 1.35355 0.646447L5 4.29289L8.64645 0.646447C8.84171 0.451184 9.15829 0.451184 9.35355 0.646447C9.54882 0.841709 9.54882 1.15829 9.35355 1.35355L5.35355 5.35355L5 5.70711L4.64645 5.35355L0.646447 1.35355C0.451184 1.15829 0.451184 0.841709 0.646447 0.646447Z"
+                        fill="#454555" />
+                </svg>
+            </div>
+        </div>
+        <ul v-if="isOpen" class="custom-options open-above shadow-lg">
+            <template v-for="(group, index) in groupedOptions" :key="index">
+                <li v-if="group.options.length > 0" class="group-label">{{ group.label }}</li>
+                <li v-for="option in group.options" :key="option.value" @click="handleOptionClick(option)">
+                    {{ option.label }}
+                </li>
+            </template>
+        </ul>
+    </div>
+</template>
+
+
+<script>
+export default {
+    data() {
+        return {
+            selectedOption: null,
+            isOpen: false,
+            options: [
+                { value: 'amazon', label: 'Amazon DSP', group: 'DSP COMPLIANCE' },
+                { value: 'walmart', label: 'Walmart DSP', group: 'DSP COMPLIANCE' },
+                { value: 'nbcu', label: 'NBCU', group: 'CTV INVENTORY COMPLIANCE' },
+                { value: 'netflix', label: 'Netflix', group: 'CTV INVENTORY COMPLIANCE' },
+                { value: 'hulu', label: 'Hulu', group: 'CTV INVENTORY COMPLIANCE' }
+            ]
+        };
+    },
+    computed: {
+        groupedOptions() {
+            // Group options by 'group' property
+            const grouped = {};
+            this.options.forEach(option => {
+                if (!grouped[option.group]) {
+                    grouped[option.group] = [];
+                }
+                grouped[option.group].push(option);
+            });
+            // Convert object to array of objects with 'label' and 'options' properties
+            return Object.keys(grouped).map(key => ({
+                label: key,
+                options: grouped[key]
+            }));
+        }
+    },
+    watch: {
+        value(newValue) {
+            this.selectedOption = newValue; // Update selectedOption when prop value changes
+        }
+    },
+    methods: {
+        toggleDropdown() {
+            this.isOpen = !this.isOpen;
+        },
+        handleOptionClick(option) {
+            this.selectedOption = option;
+            this.isOpen = false; // Close the dropdown after selection
+            this.$emit('option-selected', option); // Emit selected option to parent component
+        }
+    }
+};
+</script>
+
+<style scoped>
+.select-container {
+    position: relative;
+    display: inline-block;
+}
+
+.custom-select {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    cursor: pointer;
+    width: 100%;
+    padding: 0.75rem;
+    border: 1px solid #E7E8EB;
+    border-radius: 0.375rem;
+    background-color: white;
+}
+
+.custom-options {
+    position: absolute;
+    top: auto;
+    bottom: 100%;
+    left: 0;
+    width: 100%;
+    max-height: 120rem;
+    overflow-y: auto;
+    list-style-type: none;
+    padding: 0;
+    margin-bottom: 4px;
+    border: 1px solid #E7E8EB;
+    border-top: none;
+    border-radius: 8px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    background-color: white;
+}
+
+.custom-options li {
+    padding: 0.50rem 1rem;
+    cursor: pointer;
+    font-size: 13px;
+    font-weight: 400;
+    transition: background-color 0.3s;
+}
+
+.custom-options li:hover {
+    background-color: #f0f0f0;
+}
+
+.group-label {
+    font-weight: 500;
+    border-top: 1px solid #E7E8EB;
+    color: #818391;
+}
+
+.carrot-icon {
+    flex-shrink: 0;
+    margin-right: 4px;
+}
+
+.selected-option {
+    flex-grow: 1;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    font-weight: 400;
+    color: #818391;
+}
+
+.rotate-90 {
+    transform: rotate(-90deg);
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,37 +2,34 @@
 	<div class="min-h-screen flex flex-col items-center justify-center">
 		<TopHeader />
 
-		<main class="flex flex-col items-center justify-center flex-grow px-8">
-			<h1 class="text-3xl font-bold mb-4 text-center">
-				Real time accessibility scanning and monitoring
+		<main class="flex flex-col items-center justify-center flex-grow px-8 py-12">
+			<h1 class="text-4xl font-bold mb-4 text-center">
+				Real time creative compliance scanning and monitoring
 			</h1>
 			<p class="text-gray-700 mb-8 text-center">
-				Scan your generated tag to learn if it meets conformance standards.
+				Scan your Ad tag to learn if it meets the desired compliance standards.
 			</p>
-			<div class="flex items-center w-full max-w-md bg-fog-gray p-2 rounded-lg shadow">
-				<input
-					v-model="tag"
-					type="text"
-					placeholder="Paste tag"
-					class="w-full p-3 border-0 rounded-lg focus:outline-none focus:ring-0 text-gray-600 h-12 bg-white mr-2"
-				/>
-				<button
-					@click="startScan"
-					class="flex items-center bg-purple-500 text-white text-sm py-3 px-4 rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500 max-w-full w-[80%]"
-				>
-					<svg
-						class="h-5 w-5 mr-1 flex-shrink-0"
-						viewBox="0 -1 18 18"
-						fill="none"
-						xmlns="http://www.w3.org/2000/svg"
-					>
+			<div class="w-full max-w-3xl mt-6">
+				<textarea v-model="tag"
+					placeholder="Paste the generated tag here and select the guideline before clicking the 'Start AdGuard Scan' button."
+					class="w-full h-80 p-4 rounded-lg focus:outline-none focus:ring-2 text-gray-700 resize-none bg-fog-gray placeholder-center"></textarea>
+			</div>
+			<div class="w-full max-w-3xl flex justify-between items-center mt-2">
+				<!-- <select v-model="guideline"
+					class="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 text-gray-700 mr-2 h-15">
+					<option value="" disabled>Select Guideline</option>
+					<option value="guideline1">Guideline 1</option>
+					<option value="guideline2">Guideline 2</option>
+				</select> -->
+				<CustomDropdown :value="selectedOption" @option-selected="handleOptionSelected"></CustomDropdown>
+				<button @click="startScan"
+					class="flex items-center bg-purple-500 text-white text-sm py-3 px-4 rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500 ml-2 w-4/12">
+					<svg class="h-5 w-5 mr-1 flex-shrink-0" viewBox="0 -1 18 18" fill="none"
+						xmlns="http://www.w3.org/2000/svg">
 						<g clip-path="url(#clip0_37_170)">
-							<path
-								fill-rule="evenodd"
-								clip-rule="evenodd"
+							<path fill-rule="evenodd" clip-rule="evenodd"
 								d="M2 0L3 2L2 4L4 3L6 4L5 2L6 0L4 1L2 0ZM12 4L14 3L16 4L15 2L16 0L14 1L12 0L13 2L12 4ZM12 14L13 12L12 10L14 11L16 10L15 12L16 14L14 13L12 14ZM8.93918 4.93899L6.29248 7.58569L8.4138 9.70701L11.0605 7.06031L8.93918 4.93899ZM0.000732422 13.8784L5.58688 8.29227L7.7082 10.4136L2.12205 15.9997L0.000732422 13.8784Z"
-								fill="white"
-							/>
+								fill="white" />
 						</g>
 						<defs>
 							<clipPath id="clip0_37_170">
@@ -40,7 +37,7 @@
 							</clipPath>
 						</defs>
 					</svg>
-					<span class="ml-1"> Start AdGuard Scan </span>
+					<span class="ml-1">Start AdGuard Scan</span>
 				</button>
 			</div>
 			<div v-if="validationError" class="text-red-500 mt-2">{{ validationError }}</div>
@@ -50,16 +47,20 @@
 
 <script>
 import TopHeader from "./../components/TopHeader.vue";
+import CustomDropdown from "./../components/CustomDropdown.vue";
 
 export default {
 	name: "HomeView",
 	components: {
 		TopHeader,
+		CustomDropdown,
 	},
 	data() {
 		return {
 			tag: '',
+			guideline: '',
 			validationError: '',
+			selectedOption: '',
 		};
 	},
 	methods: {
@@ -68,13 +69,26 @@ export default {
 				this.validationError = 'Please enter a tag before starting the scan. (use "test")';
 				return;
 			}
+			if (!this.selectedOption) {
+				this.validationError = 'Please select a guideline before starting the scan.';
+				return;
+			}
 			this.validationError = '';
-			this.$router.push({ name: "Compliance", params: { id: this.tag }});
+			this.$router.push({ name: "Compliance", params: { id: this.tag, guideline: this.selectedOption.value } });
 		},
+		handleOptionSelected(option) {
+			this.selectedOption = option;
+		}
+
 	},
 };
 </script>
 
-<style>
-/* Add any additional custom styles here */
+<style scoped>
+.placeholder-center::placeholder {
+	text-align: center;
+	transform: translateY(8rem) translateX(9rem);
+	width: 60%;
+
+}
 </style>
